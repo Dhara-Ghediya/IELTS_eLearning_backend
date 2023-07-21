08851d6ab2378940b5837f5d865d9bf5368745a3
Akshay-Shingala <131964978+Akshay-Shingala@users.noreply.github.com>
2023-07-21 11:29:39 +0530

    add question tables

--- student_app/models.py
+++ student_app/models.py
@@ -49,3 +49,35 @@ class Profile(models.Model):
 
 #     def __str__(self):
 #         return self.key
+
+class StudentTestSubmitModel(models.Model):
+    timestamp = models.DateTimeField(auto_now_add = True)
+    student = models.ForeignKey(UserModel, verbose_name = "student", on_delete = models.CASCADE)
+    
+class StudentWritingAnswers(models.Model):
+    question = models.ForeignKey(writings_model, verbose_name = "Question", on_delete = models.CASCADE)
+    answer = models.TextField(verbose_name = "Answer from student")
+    checkedQuestion=models.BooleanField(default=False)
+    studentObtainMarks = models.IntegerField(default=0)
+    
+class StudentReadingAnswers(models.Model):
+    #question = models.ForeignKey(writings_model, verbose_name = "Question", on_delete = models.CASCADE)
+    firstQuestionAnswer = models.TextField(verbose_name = "First Question Answer from student")
+    secondQuestionAnswer = models.TextField(verbose_name = "Second Question Answer from student")
+    thirdQuestionAnswer = models.TextField(verbose_name = "Third Question Answer from student")
+    fourthQuestionAnswer = models.TextField(verbose_name = "Fourth Question Answer from student")
+    fifthQuestionAnswer = models.TextField(verbose_name = "Fifth Question Answer from student")
+    checkedQuestion=models.BooleanField(default=False)
+    studentObtainMarks = models.IntegerField(default=0)
+
+class StudentListeningAnswer(models.Model):
+    #question = models.ForeignKey(listing_model, verbose_name = "Question")
+    answer=models.TextField(verbose_name="student answer")
+    checkedQuestion=models.BooleanField(default=False)
+    studentObtainMarks = models.IntegerField(default=0)
+    
+class StudentSpeakingAnswer(models.Model):
+    #question = models.ForeignKey(reading_model, verbose_name="Question")
+    answer = models.AutoField(verbose_name="student Answer") 
+    checkedQuestion=models.BooleanField(default=False)
+    studentObtainMarks = models.IntegerField(default=0)
